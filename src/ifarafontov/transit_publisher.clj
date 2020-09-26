e165da5075ac40e5e0616fd5dca7ad0da7905932
ifarafontov <your@example.com>
2020-09-26 14:16:10 +0500

    Work in progress

--- src/ifarafontov/transit_publisher.clj
+++ src/ifarafontov/transit_publisher.clj
@@ -1,5 +1,75 @@
 (ns ifarafontov.transit-publisher
-  (:gen-class))
+  (:require 
+            [clojure.java.io :as io]
+            [cognitect.transit :as transit]
+            [clojure.set :as set]
+            [com.brunobonacci.mulog.buffer :as rb]
+            [com.brunobonacci.mulog.utils :as ut]
+            [com.brunobonacci.mulog.publisher :refer [PPublisher]])
+  (:import
+   [java.nio.file Files ]
+   [com.ifarafontov NoopFlushOutputStream]
+   [java.time.format DateTimeFormatter]
+   [java.time LocalDateTime Instant ZoneId Duration]
+   [com.brunobonacci.mulog.core Flake]
+   [java.io File])
+  )
+
+(set! *warn-on-reflection* true)
+
+(def formatter (DateTimeFormatter/ofPattern "YYYYMMdd_hhmmss"))
+(defn local-timestamp []
+  (.format (LocalDateTime/ofInstant (Instant/now)
+                                    (ZoneId/systemDefault)) formatter))
+
+(def flake-write-handler (transit/write-handler "flake" #(str %)))
+(def flake-read-handler (transit/read-handler #(Flake/parseFlake %)))
+
+(defn too-old? [^Instant created-at ^Instant now max-age-ms]
+  (> (.toMillis (Duration/between created-at now)) max-age-ms))
+
+(defn too-big? [size-bs max-size-bs]
+  (> size-bs max-size-bs))
+
+(defn rotate? [^File file  ^Instant created-at rotate-opts]
+  (let [[max-age-ms max-size-bs] rotate-opts]
+    (or (when max-age-ms (too-old? created-at (Instant/now) max-age-ms))
+        (when max-size-bs (too-big? (.length file) max-size-bs)))))
+
+(defn creation-time [file]
+  (.toInstant (.creationTime
+               (java.nio.file.Files/readAttributes
+                (.toPath file)
+                java.nio.file.attribute.BasicFileAttributes
+                (into-array java.nio.file.LinkOption [])))))
+
+(def time-units (let [seconds 1000
+                      minutes (* 60 seconds)
+                      hours (* 60 minutes)
+                      days (* 24 hours)
+                      weeks (* 7 days)]
+                  {:seconds seconds
+                   :minutes minutes
+                   :hours hours
+                   :days days
+                   :weeks weeks}))
+
+(def size-units (let [kb 1024
+                      mb (* 1024 kb)
+                      gb (* 1024 mb)]
+                  {:kb kb
+                   :mb mb
+                   :gb gb}))
+
+(defn descriptor->value [descriptor units-table]
+  {:pre [(map? descriptor)
+         (not-empty descriptor)
+         (set/subset? (set (keys descriptor))
+                      (set (keys units-table)))
+         (every? pos-int? (vals descriptor))]}
+  (reduce (fn [acc [unit v]] (+ acc (* (unit units-table) v)))
+          0 (seq descriptor)))
+
 
 (defn -main
   "I don't do a whole lot ... yet."
